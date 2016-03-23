ead613fa30a06640534a95779dd37b7986d46568
sietse <sietse.f@hotmail.com>
2016-03-23 01:07:44 +0100

    edituser klaar

--- Scripts/SOScript.js
+++ Scripts/SOScript.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    // voor beide tabellen (viewusers & viewstudents)
-    $('#ViewUserTable, #ViewStudentTable').DataTable({
+    // voor VieuwUserTable
+    $('#ViewUserTable').DataTable({
         language: {
             "sProcessing": "Bezig...",
             "sLengthMenu": "_MENU_ resultaten weergeven",
@@ -27,13 +27,37 @@ $(document).ready(function () {
             { "orderable": false, "targets": 6 }
 
             ]
+            
+            });
+
+     $('#ViewStudentTable').DataTable({
+        language: {
+            "sProcessing": "Bezig...",
+            "sLengthMenu": "_MENU_ resultaten weergeven",
+            "sZeroRecords": "Geen resultaten gevonden",
+            "sInfo": "_START_ tot _END_ van _TOTAL_ resultaten",
+            "sInfoEmpty": "Geen resultaten om weer te geven",
+            "sInfoFiltered": " (gefilterd uit _MAX_ resultaten)",
+            "sInfoPostFix": "",
+            "sSearch": "Zoeken:",
+            "sEmptyTable": "Geen resultaten aanwezig in de tabel",
+            "sInfoThousands": ".",
+            "sLoadingRecords": "Een moment geduld aub - bezig met laden...",
+            "oPaginate": {
+                "sFirst": "Eerste",
+                "sLast": "Laatste",
+                "sNext": "Volgende",
+                "sPrevious": "Vorige"
+            },
+            responsive: true
+
+        },
+        "columnDefs": [
+            { "orderable": false, "targets": 6 },
+            { "orderable": false, "targets": 7 }
+
+            ]
     });
-
-
-   
-       
-    
-
 
     var windowHeigth = window.innerHeight - 150;
 
